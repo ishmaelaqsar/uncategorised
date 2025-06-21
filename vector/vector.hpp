755e27071511fd57bc04f3310c14bffe399b0df5
Ishmael <isi_786@hotmail.com>
2025-06-21 16:37:03 +0100

    Update vector.hpp

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,189 +1,216 @@
-#ifndef collection_h
-#define collection_h
-
+#ifndef VECTOR_H
+#define VECTOR_H
 #include <initializer_list>
-#include <stdexcept>
 #include <type_traits>
+#include <utility>
+#include <stdexcept>
 
 namespace local
 {
+        constexpr size_t DEFAULT_SIZE = 10;
 
-template<typename T>
-class collection
-{
-public:
-    virtual ~collection() {}
-    virtual T* begin() = 0;
-    virtual T* begin() const = 0;
-    virtual T* end() = 0;
-    virtual T* end() const = 0;
-    virtual int size() const = 0;
-    virtual void push_back(const T&) = 0;
-    virtual T& operator[](int) = 0;
-    virtual const T& operator[](int) const = 0;
-};
+        template<typename T>
+        class vector
+        {
+        public:
+                vector() : vector(DEFAULT_SIZE) {}
 
-template<typename T>
-class vector : public collection<T>
-{
-private:
-    static constexpr int DEFAULT_SIZE_ = 8;
+                explicit vector(const size_t capacity) : capacity_{capacity}, size_{0}
+                {
+                        data_ = static_cast<T *>(::operator new(sizeof(T) * capacity));
+                }
 
-    int capacity_;
-    int size_;
-    T* data_;
+                vector(std::initializer_list<T> init)
+                {
+                        const auto size = static_cast<int>(init.size());
+                        capacity_ = size;
+                        size_ = size;
+                        data_ = static_cast<T *>(::operator new(sizeof(T) * size));
+                        size_t i = 0;
+                        for (const auto &element: init) {
+                                new(data_ + i) T(element);
+                                i++;
+                        }
+                }
 
-public:
-    vector() : vector(DEFAULT_SIZE_) {}
+                vector(const vector &other) : capacity_{other.capacity_}, size_{other.size_}
+                {
+                        data_ = static_cast<T *>(::operator new(sizeof(T) * other.capacity_));
+                        for (size_t i = 0; i < size_; i++) {
+                                new(data_ + i) T(other.data_[i]);
+                        }
+                }
 
-    vector(int capacity)
-        : capacity_{capacity},
-          size_{0},
-          data_{new T[capacity]} {}
+                vector(vector &&other) noexcept : capacity_{other.capacity_}, size_{other.size_}, data_{other.data_}
+                {
+                        other.capacity_ = 0;
+                        other.size_ = 0;
+                        other.data_ = nullptr;
+                }
 
-    vector(std::initializer_list<T> init)
-        : capacity_{static_cast<int>(init.size())},
-          size_{static_cast<int>(init.size())},
-          data_{new T[init.size()]}
-    {
-        std::copy(init.begin(), init.end(), data_);
-    };
+                ~vector()
+                {
+                        for (size_t i = 0; i < size_; i++) {
+                                data_[i].~T();
+                        }
+                        ::operator delete(data_);
+                }
 
-    ~vector()
-    {
-        capacity_ = 0;
-        size_ = 0;
-        delete[] data_;
-    }
+                vector &operator=(const vector &other)
+                {
+                        if (this != &other) {
+                                auto *new_data = static_cast<T *>(::operator new(sizeof(T) * other.capacity_));
+                                for (size_t i = 0; i < other.size_; i++) {
+                                        new(new_data + i) T(other.data_[i]);
+                                }
 
-    vector(const vector& o)
-        : capacity_{o.capacity()},
-          size_{o.size()},
-          data_{new T[o.capacity()]}
-    {
-        for (int i = 0; i != o.size(); ++i) {
-            data_[i] = o[i];
-        }
-    }
+                                for (size_t i = 0; i < size_; i++) {
+                                        data_[i].~T();
+                                }
+                                ::operator delete(data_);
 
-    vector<T>& operator=(const vector& o)
-    {
-        delete[] data_;
-        data_ = new T[o.capacity()];
-        capacity_ = o.capacity();
-        size_ = o.size();
-        for (int i = 0; i != o.size(); ++i) {
-            data_[i] = o[i];
-        }
-        return *this;
-    }
+                                capacity_ = other.capacity_;
+                                size_ = other.size_;
+                                data_ = new_data;
+                        }
+                        return *this;
+                }
 
-    vector(vector&& o)
-        : capacity_{o.capacity()},
-          size_{o.size()},
-          data_{o.data_}
-    {
-        o.capacity(0);
-        o.size(0);
-        o.data(nullptr);
-    }
+                vector &operator=(vector &&other) noexcept
+                {
+                        if (this != &other) {
+                                for (size_t i = 0; i < size_; i++) {
+                                        data_[i].~T();
+                                }
+                                ::operator delete(data_);
+                                capacity_ = other.capacity_;
+                                size_ = other.size_;
+                                data_ = other.data_;
+                                other.capacity_ = 0;
+                                other.size_ = 0;
+                                other.data_ = nullptr;
+                        }
+                        return *this;
+                }
 
-    vector<T>& operator=(vector&& o)
-    {
-        delete[] data_;
-        data_ = o.data_;
-        capacity_ = o.capacity();
-        size_ = o.size();
-        o.data(nullptr);
-        o.size(0);
-        return *this;
-    }
+                T &operator[](const size_t index)
+                {
+                        if (index >= size_) {
+                                throw std::out_of_range("index out of range");
+                        }
+                        return data_[index];
+                }
 
-    T* begin() override
-    {
-        return size() ? &data_[0] : nullptr;
-    }
+                const T &operator[](const size_t index) const
+                {
+                        if (index >= size_) {
+                                throw std::out_of_range("index out of range");
+                        }
+                        return data_[index];
+                }
 
-    T* begin() const override
-    {
-        return size() ? &data_[0] : nullptr;
-    }
+                explicit operator bool() const
+                {
+                        return size_ > 0;
+                }
 
-    T* end() override
-    {
-        return size() ? &data_[0] + size() : nullptr;
-    }
+                T *begin()
+                {
+                        return data_;
+                }
 
-    T* end() const override
-    {
-        return size() ? &data_[0] + size() : nullptr;
-    }
+                const T *begin() const
+                {
+                        return data_;
+                }
 
-    int size() const override
-    {
-        return size_;
-    }
+                T *end()
+                {
+                        return data_ + size_;
+                }
 
-    void push_back(const T& item) override
-    {
-        if (size_ < capacity_) {
-            data_[size_++] = item;
-        } else {
-            T* n_data = new T[capacity_ * 2];
-            std::copy(this->begin(), this->end(), n_data);
-            delete[] data_;
-            data_ = n_data;
-            capacity_ = capacity_ * 2;
-            this->push_back(item);
-        }
-    }
+                const T *end() const
+                {
+                        return data_ + size_;
+                }
 
-    T& operator[](int index) override
-    {
-        if (index < 0 || index >= size_) {
-            throw std::out_of_range("invalid index");
-        }
-        return data_[index];
-    }
+                size_t capacity() const
+                {
+                        return capacity_;
+                }
 
-    const T& operator[](int index) const override
-    {
-        if (index < 0 || index >= size_) {
-            throw std::out_of_range("invalid index");
-        }
-        return data_[index];
-    }
+                size_t size() const
+                {
+                        return size_;
+                }
 
-    int capacity() const
-    {
-        return capacity_;
-    }
+                bool empty() const
+                {
+                        return size_ == 0;
+                }
 
-    void capacity(int capacity)
-    {
-        capacity_ = capacity;
-    }
+                void push_back(const T &element)
+                {
+                        ensure_capacity(size_ + 1);
+                        new(data_ + size_) T(element);
+                        size_++;
+                }
 
-    void size(int size)
-    {
-        size_ = size;
-    }
+                void push_back(T &&element)
+                {
+                        ensure_capacity(size_ + 1);
+                        new(data_ + size_) T(std::move(element));
+                        size_++;
+                }
 
-    void data(T* data)
-    {
-        data_ = data;
-    }
+                void pop_back()
+                {
+                        if (size_ > 0) {
+                                size_--;
+                                data_[size_].~T();
+                        }
+                }
 
-    template<typename F, typename ResultType = std::invoke_result_t<F, T>>
-    vector<ResultType> map(const F&& func) const
-    {
-        vector<ResultType> r(size_);        
-        for (const auto& elem : *this) {
-            r.push_back(func(elem));
-        }
-        return r;
-    }
-};
+                template<typename F, typename ResultType = std::invoke_result_t<F, T> >
+                vector<ResultType> map(F &&func)
+                {
+                        vector<ResultType> r(size_);
+                        for (const auto &elem: *this) {
+                                r.push_back(func(elem));
+                        }
+                        return r;
+                }
 
+                template<typename F, typename ResultType = std::invoke_result_t<F, T> >
+                vector<ResultType> map(F &&func) const
+                {
+                        vector<ResultType> r(size_);
+                        for (const auto &elem: *this) {
+                                r.push_back(func(elem));
+                        }
+                        return r;
+                }
+
+        private:
+                size_t capacity_;
+                size_t size_;
+                T *data_;
+
+                void ensure_capacity(const size_t required_capacity)
+                {
+                        if (capacity_ < required_capacity) {
+                                const size_t new_capacity = std::max(1ULL, required_capacity * 2);
+                                auto new_data = static_cast<T *>(::operator new(sizeof(T) * new_capacity));
+                                for (size_t i = 0; i < size_; i++) {
+                                        new(new_data + i) T(std::move(data_[i]));
+                                        data_[i].~T();
+                                }
+                                ::operator delete(data_);
+                                capacity_ = new_capacity;
+                                data_ = new_data;
+                        }
+                }
+        };
 }
-#endif
+
+#endif //VECTOR_H
